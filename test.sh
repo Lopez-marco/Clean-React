fc39409072cdc22f24e6d1a01e9f2e6a528cca9a
Marco Lopez <lopez.marcohdez@gmail.com>
2022-11-10 11:04:06 -0500

    Updating Clean-React app

---
+++ test.sh
@@ -0,0 +1,12 @@
+git add .
+
+echo "Enter commit mesage:" 
+read commitMessage
+git commit -m "$commitMessage"
+
+echo "Enter the name of the branch:"
+read branch
+
+git push origin $branch
+
+read
